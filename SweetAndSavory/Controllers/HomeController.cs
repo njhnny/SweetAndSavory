186e48dc1806bd9350d024d794eea5588b94c11b
Nick Hennessy <njhnny@gmail.com>
2021-10-31 19:09:57 -0700

    update home coontroller

--- SweetAndSavory/Controllers/HomeController.cs
+++ SweetAndSavory/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using SweetAndSavory.Models;
+using System.Linq;
 
 namespace SweetAndSavory.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly SweetAndSavoryContext _db;
+    public HomeController(SweetAndSavoryContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      dynamic model = new ExpandoObject();
+      model.Flavors = _db.Flavors.ToList();
+      model.Treats = _db.Treats.ToList();
+      return View(model);
+    }
+  }
 }
